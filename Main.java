9d0c527746a238159b54454076b005e5cb97a8d2
skmryut <y.sekimura.sys22@morijyobi.ac.jp>
2022-11-08 12:23:32 +0900

    ２人分のBMIの計算

--- Main.java
+++ Main.java
@@ -6,6 +6,11 @@ public class Main{
         double weight=s.nextDouble();
         String result=String.format("%.2f",weight/(height*height));
         System.out.println("Ç†Ç»ÇΩÇÃBMI"+result+"Ç≈Ç∑ÅB");
+        double height2=s.nextDouble();
+        double weight2=s.nextDouble();
+        String result2=String.format("%.2f",weight2/(height2*height2));
+        System.out.println("Ç†Ç»ÇΩÇÃBMI"+result2+"Ç≈Ç∑ÅB");
+
     }
 }
 
